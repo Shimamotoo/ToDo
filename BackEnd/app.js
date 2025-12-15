4b4d89a9a07ee7b42f6a43957531b912defef91b
BrunoMS <bruno.mitsuhiroo@gmail.com>
2025-12-15 12:04:14 -0300

    Rota para adicionar tarefa

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,20 +1,39 @@
+//Express: framework para Node.js que facilita a criação de servidores web
+
+//Servidor HTTP: Abre uma porta | Recebe requisições HTTP | Envia respostas
+
+//Aplicação Express: Guarda todas as rotas | Guarda os middlewares | Decide como responder às requisições
+
+// "express" => criar um aplicação | "{json}" => serve para ler o json que vem da requisição
 import express, { json } from "express";
+
+//Permite que o frontend em outra porta/domínio acesse o backend
 import cors from "cors";
+
+//Faz a importação da conexão com o banco
 import db from "./database/db.js";
+
+//Importa o arquivo de rotas de tarefas
 import taskRoutes from "./routes/taskRoutes.js";
 
-
+//Cria a aplicação Express, que encapsula o servidor HTTP
 const app = express();
 
+//Ativa CORS para todas as rotas
 app.use(cors());
+
+//Leitura de JSON
 app.use(json());
+
+//Toda requisição que começar com "/tasks" ira executar o (router) do taskRoutes
 app.use("/tasks", taskRoutes);
 
-
+//Cria um rota simples com o intuito de testar se o servidor está rodando 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando!");
 });
 
+//Testa a conexão com o banco de dados(Essa rota é temporária, usada só para debug) | REMOVER DEPOIS
 app.get("/teste-db", (req, res) => {
   db.query("SELECT 1 + 1 AS resultado", (err, rows) => {
     if (err) return res.status(500).send(err);
@@ -22,6 +41,7 @@ app.get("/teste-db", (req, res) => {
   });
 });
 
+//Abre a porta 3000 e começa a “escutar” requisições HTTP
 app.listen(3000, () => {
   console.log("Server rodando na porta 3000");
 });
--- BackEnd/routes/taskRoutes.js
+++ BackEnd/routes/taskRoutes.js
@@ -17,19 +17,53 @@ router.get("/", (req, res) => {
     /**
      * Query para ser executada dentro do banco
      *  */     
-    const sql = "SELECT * FROM tasks";
+    const sqlQuery = "SELECT * FROM tasks";
 
     //db.query envia a query para o banco de dados
     //Método assíncrono, envia a query e espera o retorno dela
     //(err) = caso de erro, console.error(err) faz o console do erro
     //(results) = retorna um JSON com o resultado da query
-    db.query(sql, (err, results) => {
+    db.query(sqlQuery, (err, results) => {
             if (err) {
             console.error(err);
             return res.status(500).json({ error: "Erro ao buscar tarefas" });
         }
 
     res.json(results);
+  });
+});
+
+
+router.post("/", (req, res) => {
+
+  //Desestrutura o req.body e pegando apenas o campo titulo
+  const {titulo} = req.body;
+
+  //Caso o titulo não tenha sido informado retorna um erro
+  if(!titulo) {
+    return res.status(400).json({ erro: "Título da tarefa é obrigatório." });
+  }
+
+  //Variavel que contem a query que sera enviada ao banco
+  const sqlQuery = "INSERT INTO tasks (titulo) VALUES (?)";
+
+  //Envia a query para o banco de dados
+  //Envia o valor do titulo
+  //Método assíncrono, envia a query e espera o retorno dela
+  db.query(sqlQuery, [titulo],(err, result) => {
+
+    //(err) = caso de erro, console.error(err) faz o console do erro
+    if(err){
+      console.error(err);
+      return res.status(500).json({ error:"Erro ao adicionar tarefa" });
+    }
+
+    //Retorna ao cliente o id, titulo e se está completo a tarefa
+    res.status(201).json({
+      id:result.insertId,
+      titulo,
+      completo: false,
+    });
   });
 });
 
